8456bb49c5284d347e0f154e68168700c25dec3b
StalynAlvarado <136040944+StalynAlvarado@users.noreply.github.com>
2024-02-29 23:54:18 -0500

    Update Categoria.java

--- src/main/java/com/example/demo/model/Categoria.java
+++ src/main/java/com/example/demo/model/Categoria.java
@@ -1,3 +1,14 @@
+package com.example.demo.model;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Categoria{
+  	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer idCategoria;
+  @Column(nullable = false)
+  private String nombre;
   
 }
