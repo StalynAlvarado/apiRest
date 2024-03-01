b35637b14275006db392a1faf3e85d5dbf47449b
jhon <jhon@gmail.com>
2024-02-29 23:38:05 -0500

    usando lombok

--- src/main/java/com/example/demo/model/Producto.java
+++ src/main/java/com/example/demo/model/Producto.java
@@ -6,12 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="Productos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Producto {
 
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer idPro;
@@ -24,70 +29,7 @@ public class Producto {
 	@Column
 	private String descripcion;
 	
-	public Producto() {
 	
-	}
-	
-	public Producto(Producto producto)
-	{
-		this(producto.getIdPro(),producto.getNombre(),producto.getPrecio(),producto.getTipo(),
-				producto.getDescripcion());
-	}
-
-
-
-	public Producto(Integer idPro, String nombre, double precio, String tipo, String descripcion) {
-		super();
-		this.idPro = idPro;
-		this.nombre = nombre;
-		this.precio = precio;
-		this.tipo = tipo;
-		this.descripcion = descripcion;
-	}
-	
-	
-
-	public Integer getIdPro() {
-		return idPro;
-	}
-
-	public void setIdPro(Integer idPro) {
-		this.idPro = idPro;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public double getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
-
-	public String getTipo() {
-		return tipo;
-	}
-
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-
-	public String getDescripcion() {
-		return descripcion;
-	}
-
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-
-
 
 
 	
